b580bfe2253d1217aae0565e3b1225a0bfed3abc
Patrick Joyce <pjaudiomv@gmail.com>
2022-01-30 20:10:29 -0500

    pj dog

--- src/node/index.js
+++ src/node/index.js
@@ -51,6 +51,10 @@ fetch('https://raw.githubusercontent.com/bmlt-enabled/tomato/master/rootServerLi
         ask(chalk.magentaBright('Select a root server: '))
             .then(rootserverSelected => {
                 log('')
+                if (parseInt(rootserverSelected) > rootServers.length) {
+                    log(chalk.redBright("Error: Selection must match one of the choices."))
+                    process.exit(0)
+                }
                 let root_server_selected_url = rootServers[rootserverSelected-1]["rootURL"];
                 let root_server_selected_name = rootServers[rootserverSelected-1]["name"];
                 log(chalk.magentaBright('You Selected:'), chalk.redBright(rootserverSelected, '[' + root_server_selected_name + ']'))
@@ -61,29 +65,34 @@ fetch('https://raw.githubusercontent.com/bmlt-enabled/tomato/master/rootServerLi
                 log(chalk.magentaBright('Regions:'))
                 fetch(rootServerUrl + "client_interface/json/?switcher=GetServiceBodies")
                     .then(res => res.text())
-                    .then((regionListRaw) => {
-                        let regionList = JSON.parse(regionListRaw).sort(function(a, b) {
+                    .then((serviceBodiesRaw) => {
+                        let serviceBodiesList = JSON.parse(serviceBodiesRaw).sort(function(a, b) {
                             a = a.name.toLowerCase();
                             b = b.name.toLowerCase();
                             return (a < b) ? -1 : (a > b) ? 1 : 0;
                         });
+
+                        return serviceBodiesList
+                    })
+                    .then(serviceBodies => {
+                        log('')
                         let rl = 1
-                        let regionArr = [];
-                        for (const region of regionList) {
+                        let regions = [];
+                        for (const region of serviceBodies) {
                             if (region.type.includes("RS")) {
                                 let regionName = region.name;
                                 log(chalk.yellowBright(' ', rl, '', regionName))
-                                regionArr.push(region);
+                                regions.push(region);
                                 rl++
                             }
                         }
-                        return regionArr
-                    })
-                    .then(regions => {
-                        log('')
                         ask(chalk.magentaBright('Select a region: '))
                             .then(regionSelected => {
                                 log('')
+                                if (parseInt(regionSelected) > regions.length) {
+                                    log(chalk.redBright("Error: Selection must match one of the choices."))
+                                    process.exit(0)
+                                }
                                 let region_selected_id = regions[regionSelected-1]["id"];
                                 let region_selected_name = regions[regionSelected-1]["name"];
                                 log(chalk.magentaBright('You Selected:'), chalk.redBright(regionSelected,
@@ -93,7 +102,30 @@ fetch('https://raw.githubusercontent.com/bmlt-enabled/tomato/master/rootServerLi
                             .then(regionId => {
                                 log('')
                                 log(chalk.magentaBright('Service Bodies:'))
-                                log(chalk.magentaBright(' ',rootServerUrl, ' ',  regionId))
+                                // console.log(serviceBodies)
+
+                                log(chalk.magentaBright(' ', regionId, '',rootServerUrl))
+                                // fetch(rootServerUrl
+                                //     + "client_interface/json/?switcher=GetSearchResults&services="
+                                // + regionId + "&recursive=1&data_field_key=formats")
+                                //     .then(res => res.text())
+                                //     .then((meetingsRaw) => {
+                                //         let meetings = JSON.parse(meetingsRaw)
+                                //         let ml = 0
+                                //         let meetingArr = [];
+                                //         for (const meeting of meetings) {
+                                //             console.log(meeting)
+                                //
+                                //             ml++
+                                //             // if (region.type.includes("RS")) {
+                                //             //     let regionName = region.name;
+                                //             //     log(chalk.yellowBright(' ', rl, '', regionName))
+                                //             //     regionArr.push(region);
+                                //             //     rl++
+                                //             // }
+                                //         }
+                                //     })
+                                //log(chalk.magentaBright(' ',rootServerUrl, ' ',  regionId))
 
                             })
                     })
