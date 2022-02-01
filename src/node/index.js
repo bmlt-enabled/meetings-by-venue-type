1e821500b4b9e9607fb51099e2b31e71c96096cf
pjaudiomv <34245618+pjaudiomv@users.noreply.github.com>
2022-01-31 22:05:49 -0500

    update (#10)

--- src/node/index.js
+++ src/node/index.js
@@ -11,14 +11,16 @@ const rl = readline.createInterface({
 });
 rl.pause();
 
-let PRPL = "\u001b[1;35m";
-let WHT = "\u001b[1;38m";
-let YLW = "\u001b[1;93m";
-let RED = "\u001b[1;31m";
-let GRN = "\u001b[1;32m";
-let TAL = "\u001b[1;36m";
-let BLU = "\u001b[1;34m";
-let EC = "\u001b[0m";
+class Color {
+  static PRPL = "\u001b[1;35m";
+  static WHT = "\u001b[1;38m";
+  static YLW = "\u001b[1;93m";
+  static RED = "\u001b[1;31m";
+  static GRN = "\u001b[1;32m";
+  static TAL = "\u001b[1;36m";
+  static BLU = "\u001b[1;34m";
+  static EC = "\u001b[0m";
+}
 
 async function fetch(url) {
   return new Promise(async (resolve, reject) => {
@@ -39,8 +41,8 @@ async function fetch(url) {
 }
 
 log("");
-log(WHT + "Get Meetings By Venue-Type", EC, "\n");
-log(PRPL + "Root Servers:", EC);
+log(Color.WHT + "Get Meetings By Venue-Type", Color.EC, "\n");
+log(Color.PRPL + "Root Servers:", Color.EC);
 
 const tomatoServersData = await fetch(
   "https://raw.githubusercontent.com/bmlt-enabled/tomato/master/rootServerList.json"
@@ -56,19 +58,18 @@ let tomatoRootServers = tomatoServersData.sort(function (a, b) {
 let rs = 1;
 for (const rootServer of tomatoRootServers) {
   let rootServerName = rootServer.name;
-  log(YLW + " " + rs, "", rootServerName, EC);
+  log(Color.YLW + " " + rs, "", rootServerName, Color.EC);
   rs++;
 }
-log("");
 
+log("");
 const rootServerSelected = await new Promise((resolve) => {
-  rl.question(PRPL + "Select a root server: " + EC, resolve);
+  rl.question(Color.PRPL + "Select a root server: " + Color.EC, resolve);
 });
 
 log("");
-
 if (parseInt(rootServerSelected) > tomatoRootServers.length) {
-  log(RED + "Error: Selection must match one of the choices.", EC);
+  log(Color.RED + "Error: Selection must match one of the choices.", Color.EC);
   process.exit(0);
 }
 let root_server_selected_url =
@@ -76,13 +77,13 @@ let root_server_selected_url =
 let root_server_selected_name =
   tomatoRootServers[rootServerSelected - 1]["name"];
 log(
-  PRPL + "You Selected:" + EC,
-  RED + rootServerSelected,
+  Color.PRPL + "You Selected:" + Color.EC,
+  Color.RED + rootServerSelected,
   "[" + root_server_selected_name + "]",
-  EC
+  Color.EC
 );
 log("");
-log(PRPL + "Regions:", EC);
+log(Color.PRPL + "Regions:", Color.EC);
 
 const rootServersData = await fetch(
   root_server_selected_url + "client_interface/json/?switcher=GetServiceBodies"
@@ -100,7 +101,7 @@ let regions = [];
 for (const region of serviceBodies) {
   if (region.type.includes("RS")) {
     let regionName = region.name;
-    log(YLW + " " + sc, "", regionName, EC);
+    log(Color.YLW + " " + sc, "", regionName, Color.EC);
     regions.push(region);
     sc++;
   }
@@ -108,26 +109,25 @@ for (const region of serviceBodies) {
 
 log("");
 const regionSelected = await new Promise((resolve) => {
-  rl.question(PRPL + "Select a region: " + EC, resolve);
+  rl.question(Color.PRPL + "Select a region: " + Color.EC, resolve);
 });
 
 log("");
-
 if (parseInt(regionSelected) > regions.length) {
-  log(RED + "Error: Selection must match one of the choices.", EC);
+  log(Color.RED + "Error: Selection must match one of the choices.", Color.EC);
   process.exit(0);
 }
 let region_selected_id = regions[regionSelected - 1]["id"];
 let region_selected_name = regions[regionSelected - 1]["name"];
 log(
-  PRPL + "You Selected:" + EC,
-  RED + regionSelected,
+  Color.PRPL + "You Selected:" + Color.EC,
+  Color.RED + regionSelected,
   "[" + region_selected_name + "]",
-  EC
+  Color.EC
 );
 
 log("");
-log(PRPL + "Service Bodies:", EC);
+log(Color.PRPL + "Service Bodies:", Color.EC);
 let sb = 1;
 let serviceBodyArr = [];
 for (const serviceBody of serviceBodies) {
@@ -136,22 +136,20 @@ for (const serviceBody of serviceBodies) {
     serviceBody.id === region_selected_id
   ) {
     let serviceBodyName = serviceBody.name;
-    log(YLW + " " + sb, "", serviceBodyName, EC);
+    log(Color.YLW + " " + sb, "", serviceBodyName, Color.EC);
     serviceBodyArr.push(serviceBody);
     sb++;
   }
 }
 
 log("");
-
 const serviceBodySelected = await new Promise((resolve) => {
-  rl.question(PRPL + "Select a service body: " + EC, resolve);
+  rl.question(Color.PRPL + "Select a service body: " + Color.EC, resolve);
 });
 
 log("");
-
 if (parseInt(serviceBodySelected) > serviceBodyArr.length) {
-  log(RED + "Error: Selection must match one of the choices.", EC);
+  log(Color.RED + "Error: Selection must match one of the choices.", Color.EC);
   process.exit(0);
 }
 
@@ -159,14 +157,13 @@ let service_body_selected_id = serviceBodyArr[serviceBodySelected - 1]["id"];
 let service_body_selected_name =
   serviceBodyArr[serviceBodySelected - 1]["name"];
 log(
-  PRPL + "You Selected:" + EC,
-  RED + serviceBodySelected,
+  Color.PRPL + "You Selected:" + Color.EC,
+  Color.RED + serviceBodySelected,
   "[" + service_body_selected_name + " (" + service_body_selected_id + ")]",
-  EC
+  Color.EC
 );
 
 log("");
-
 const meetingsData = await fetch(
   root_server_selected_url +
     "client_interface/json/?switcher=GetSearchResults&services=" +
@@ -174,14 +171,10 @@ const meetingsData = await fetch(
     "&recursive=1&data_field_key=formats"
 );
 
-let meetings = meetingsData;
-let allMeetings = 0;
-let inPerson = 0;
-let virtual = 0;
-let hybrid = 0;
-let tempVirtual = 0;
+let allMeetings, inPerson, virtual, hybrid, tempVirtual, tempClosed;
+allMeetings = inPerson = virtual = hybrid = tempVirtual = tempClosed = 0;
 
-for (const meeting of meetings) {
+for (const meeting of meetingsData) {
   let formats = meeting.formats.split(",");
   if (
     !formats.includes("VM") &&
@@ -214,16 +207,38 @@ for (const meeting of meetings) {
     formats.includes("HY")
   ) {
     hybrid++;
+  } else if (
+    !formats.includes("VM") &&
+    formats.includes("TC") &&
+    !formats.includes("HY")
+  ) {
+    tempClosed++;
   }
   allMeetings++;
 }
 
-log(WHT + "Total Meetings By Venue Type", EC, "\n");
-log(BLU + "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=", EC, "\n");
-log(TAL + "In-person: " + EC + GRN + inPerson, EC);
-log(TAL + "Hybrid: " + EC + GRN + hybrid, EC);
-log(TAL + "Virtual: " + EC + GRN + virtual, EC);
-log(WHT + "Total Meetings: " + EC + GRN + allMeetings, EC, "\n");
-log(BLU + "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=", EC, "\n");
+let totals = {
+  inPerson: inPerson,
+  virtual: virtual,
+  tempVirtual: tempVirtual,
+  hybrid: hybrid,
+  tempClosed: tempClosed,
+  allMeetings: allMeetings,
+};
+
+log(Color.WHT + "Total Meetings By Venue Type", Color.EC, "\n");
+log(Color.BLU + "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=", Color.EC, "\n");
+log(
+  Color.TAL + "In-person: " + Color.EC + Color.GRN + totals.inPerson,
+  Color.EC
+);
+log(Color.TAL + "Hybrid: " + Color.EC + Color.GRN + totals.hybrid, Color.EC);
+log(Color.TAL + "Virtual: " + Color.EC + Color.GRN + totals.virtual, Color.EC);
+log(
+  Color.WHT + "Total Meetings: " + Color.EC + Color.GRN + totals.allMeetings,
+  Color.EC,
+  "\n"
+);
+log(Color.BLU + "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=", Color.EC, "\n");
 
 rl.pause();
